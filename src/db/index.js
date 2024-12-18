8328b1163b5ae63a18ea54a3b67e9d554fc2581c
ShwetankDohroo1 <shwetankwithdohroo@gmail.com>
2024-12-19 01:48:39 +0530

    Done

--- src/db/index.js
+++ src/db/index.js
@@ -2,8 +2,6 @@ import dotenv from "dotenv";
 dotenv.config({path : "./.env"})
 import { DB_NAME } from "../constants.js";
 import mongoose from "mongoose";
-
-
 const connectDb = async()=>{
     try{
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}${DB_NAME}`)
@@ -11,9 +9,8 @@ const connectDb = async()=>{
         
     }
     catch(error){
-        console.log("Mongo DB error");
+        console.log("Mongo DB error",error);
         process.exit(1);
     }
 }
-
 export default connectDb;
