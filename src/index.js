af835ebb53ec9c7e85afedb867ad3aa309125789
ShwetankDohroo1 <shwetankwithdohroo@gmail.com>
2024-12-19 01:42:12 +0530

    Done

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ connectDb()
 .then(
     ()=>{
         const port = process.env.PORT || 8000;
-        app.listen(port,()=>{
+        app.listen(port,"0.0.0.0",()=>{
             console.log(`Server is live at port : ${port}`);
         })
     }
